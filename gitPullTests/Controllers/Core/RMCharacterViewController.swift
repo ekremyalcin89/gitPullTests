efd5aee4d454df7a81227f7e516d24fe5086518d
ekrem <yalcinekrem89@gmail.com>
2023-01-02 15:10:22 +0300

    created API requests

--- gitPullTests/Controllers/Core/RMCharacterViewController.swift
+++ gitPullTests/Controllers/Core/RMCharacterViewController.swift
@@ -11,13 +11,31 @@ final class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemBackground
         title = "Characters"
         
+        let request = RMRequest(
+            endpoint: .character,
+            queryParameters: [
+                URLQueryItem(name: "name", value: "rick"),
+                URLQueryItem(name: "status", value: "alive")
+            ]
+        )
+        print(request.url)
+        
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+            
+        }
+        
+        
     }
+        
+      
+}
     
 
   
 
-}
+

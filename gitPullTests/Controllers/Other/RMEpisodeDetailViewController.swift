8069c4e5aaeca4110a9bb565287a8f5ae2f84c61
ekrem <yalcinekrem89@gmail.com>
2023-01-25 22:32:35 +0300

    episode  info character selection added

--- gitPullTests/Controllers/Other/RMEpisodeDetailViewController.swift
+++ gitPullTests/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 // VIEW CONTROLLER TO SHOW DETAIS ---
 
-final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate {
+final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailViewViewModelDelegate, RMEpisodeDetailViewDelegate {
 
     private let viewModel: RMEpisodeDetailViewViewModel
     
@@ -32,6 +32,7 @@ final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailView
         super.viewDidLoad()
         view.addSubview(detailView)
         addConstraints()
+        detailView.delegate = self
         title = "Episode"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
         
@@ -52,6 +53,16 @@ final class RMEpisodeDetailViewController: UIViewController, RMEpisodeDetailView
     @objc private func didTapShare() {
         
     }
+    
+    func rmEpisodeDetailView(_ detailView: RMEpisodeDetailView,
+                             didSelect character: RMCharacter
+    ) {
+        let vc = RMCharacterDetailViewController(viewModel: .init(character: character))
+        vc.title = character.name
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
     func didFetchEpisodeDetails() {
         detailView.configure(with: viewModel)
     }
